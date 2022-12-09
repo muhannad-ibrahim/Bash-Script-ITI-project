e9bd80fa0f6d601fc74ddb08aa5d189380c1d93a
Mohammed Abdallah <mohammed.a.farhoud@gmail.com>
2022-12-09 20:35:18 +0200

    Updating list, drop, connect functions without checks

--- main.sh
+++ main.sh
@@ -4,22 +4,46 @@ function create_database {
 	read dbName
 	#if [[ dbName ]] #for checking if it's exist or not 	
 	mkdir ~/Bash_project/$dbName
-	echo "Database has been created successfully"
+	echo "Database $dbName has been created successfully"
 }
+
+function list_databases { 	
+	ls -d */
+}
+
+function connect_database {
+	# Just trial Not the function we want
+	echo "Enter name of the database you want to Use: "
+	read dbName
+	#if [[ dbName ]] #for checking if it's exist or not 	
+	cd ~/Bash_project/$dbName
+	ls -l
+	echo "Connected to $dbName database"	
+
+}
+
+function drop_database { 
+	echo "Enter name of the database you want to drop: "
+	read dbName
+	#if [[ dbName ]] #for checking if it's exist or not 	
+	rm -r ~/Bash_project/$dbName
+	echo "Database $dbName has been dropped successfully"	
+
+}
+
 select choice in 'Create Database' 'List Databases' 'Connect To Database' 'Drop Databse' 'Exit'
 do
 case $REPLY in
 	1) create_database
 		;;
-	2) #list_database
+	2) list_databases
 		;;
-	3) #connect_database
+	3) connect_database
 		;;
-	4) #drop_databse
+	4) drop_database
 		;;
 	5) exit 
 		;;
 	*) echo "Invaild choice"
 esac
 done
-	
