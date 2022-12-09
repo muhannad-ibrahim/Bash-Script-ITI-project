e6188b0494b1609b0f9d621e1326280e3ad2b87b
Muhannad Ibrahim <mibrahim@192.168.1.10>
2022-12-10 00:10:00 +0200

    adding the condition of if the database exist or not

--- main.sh
+++ main.sh
@@ -2,33 +2,49 @@
 function create_database {
 	echo "Enter name of the database : "
 	read dbName
-	#if [[ dbName ]] #for checking if it's exist or not 	
-	mkdir ~/Bash_project/$dbName
-	echo "Database $dbName has been created successfully"
+	mkdir -p ~/Bash_project/$dbName	
+	if [[ $? == 0 ]] #for checking if it's exist or not 	
+	then	
+		echo "Database $dbName has been created successfully"
+	else
+		echo "Error database already exists"
+	fi
 }
 
-function list_databases { 	
-	ls -d */
+function list_databases { 
+	cd ~/Bash_project 2>> /dev/null
+	if [[ $? == 0 ]]
+	then
+		ls  
+	else
+		echo "There's no any database to show, Try to create one :)"
+	fi
 }
 
 function connect_database {
 	# Just trial Not the function we want
 	echo "Enter name of the database you want to Use: "
 	read dbName
-	#if [[ dbName ]] #for checking if it's exist or not 	
-	cd ~/Bash_project/$dbName
-	ls -l
-	echo "Connected to $dbName database"	
-
+	cd ~/Bash_project/$dbName 2>> /dev/null	
+	if [[ $? == 0 ]] #for checking if it's exist or not
+	then 		
+		echo "Connected to $dbName database"
+		#tables_menu #function nedd to be implemented	
+	else
+		echo "Database $dbName not exists"
+	fi
 }
 
 function drop_database { 
 	echo "Enter name of the database you want to drop: "
 	read dbName
-	#if [[ dbName ]] #for checking if it's exist or not 	
-	rm -r ~/Bash_project/$dbName
-	echo "Database $dbName has been dropped successfully"	
-
+	rm -r ~/Bash_project/$dbName 2>> /dev/null	
+	if [[ $? == 0 ]]  	
+	then
+		echo "Database $dbName has been dropped successfully"
+	else
+		echo "Database not found"
+	fi
 }
 
 select choice in 'Create Database' 'List Databases' 'Connect To Database' 'Drop Databse' 'Exit'
