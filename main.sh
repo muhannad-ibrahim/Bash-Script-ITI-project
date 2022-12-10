33ec40b0b4a7b2e672f1d029206c4ad80732d4cc
Muhannad Ibrahim <mibrahim@192.168.1.10>
2022-12-10 22:15:14 +0200

    add on the create table function to add file of data and file of datatypes and primary key

--- main.sh
+++ main.sh
@@ -9,32 +9,41 @@ function create_database {
 	echo "Enter name of the database : "
 	read dbName
 	mkdir -p ~/Bash_project/$dbName	
-	if [[ $? == 0 ]] #for checking if it's exist or not 	
+	if [[ $? == 0 ]] 					#for checking if it's exist or not 	
 	then	
 		echo "Database $dbName has been created successfully"
 	else
 		echo "Error database $dbName already exists"
 	fi
+	main_menu
 }
 
 function create_table {
 	typeset -i colNum
 	echo "Enter name of the table : "
 	read tableName
+	
 	if [[ $tableName != +([a-zA-Z0-9_-]) ]] # in MySQL there is no naming conventions on tables we can remove this check
 	then	
 		echo "Only use characters, numbers dash and underscore"
+	
 	elif [[ -f ~/Bash_project/$dbName/$tableName ]] #for checking if it's exist or not 	
 	then	
 		echo "Error table $tableName already exists"
+	
 	else 
-		touch ~/Bash_project/$dbName/$tableName
-		echo "Table $tableName has been created successfully"
+		sep="|" 							# used to be the seperator between data  
+  		rSep="\n"							# used to new line 	
+  		pKey="" 							# used to be primary key
+  		metaData="Field"$sep"Type"$sep"key" # used to collect the tuple of data 
+
 		echo "Enter the number of columns"
 		read columnNumber
+
 		if [[ $columnNumber != +([0-9]) || $columnNumber -eq 0 ]]
 		then
 			echo "Number is not correct, please enter a positive number"
+		
 		else
 			for ((i=1; i<=$columnNumber; i++))
 			do
@@ -42,37 +51,73 @@ function create_table {
 				read colName
 				if [[ $colName != +([a-zA-Z0-9_-]) ]]
 				then
-					echo "Enter a vaild column number"
+					echo "Enter a vaild column name"
 				else
-					echo "Enter column $i Datatype"
-					echo -en "$colName:" >> ~/Bash_project/$dbName/$tableName
+					echo "Enter column $i Datatype of $colName"
 					select choice in "str" "int"
 					do
-					case $REPLY in
-					1) colDataType="str"
-						break
-						;;
-					2) colDataType="int"
-						break
-						;;
-					*) echo "Wrong data type"
-					esac
+						case $REPLY in
+							1) colDataType="str"
+								break
+								;;
+							2) colDataType="int"
+								break
+								;;
+							*) echo "Wrong data type"
+						esac
 					done
+					
+					if [[ $pKey == "" ]]; then
+					echo -e "Make PrimaryKey ? "
+					
+					select var in "yes" "no"
+					do
+						case $var in
+						yes ) pKey="PK";
+						metaData+=$rSep$colName$sep$colDataType$sep$pKey;
+						break;;
+						no )
+						metaData+=$rSep$colName$sep$colDataType$sep""
+						break;;
+						* ) echo "Wrong Choice" ;;
+						esac
+					done
+					else
+						metaData+=$rSep$colName$sep$colDataType$sep""
+					fi
+					if [[ $i == $columnNumber ]]; then
+						temp=$temp$colName
+					else
+						temp=$temp$colName$sep
+					fi
 				fi
 			done
-		fi				
+		fi	
+		touch .$tableName
+		echo -e $metaData  >> .$tableName
+		touch $tableName
+		echo -e $temp >> $tableName
+		if [[ $? == 0 ]]
+		then
+			clear
+			echo "Table Created Successfully"
+			tables_menu
+		else
+			echo "Error Creating Table $tableName"
+			tables_menu
+		fi			
 	fi
 }
 
-function list_tables { 
-	cd ~/Bash_project/$dbName 2>> /dev/null
-	if [[ $? == 0 ]]
-	then
-		ls
-	else
-		echo "There's no any tables to show, Try to create one :)"
-	fi
-}
+#function list_tables { 
+#	cd ~/Bash_project/$dbName 2>> /dev/null
+#	if [[ $? == 0 ]]
+#	then
+#		ls
+#	else
+#		echo "There's no any tables to show, Try to create one :)"
+#	fi
+#}
 
 function drop_table { 
 	echo "Enter name of the table you want to drop: "
@@ -84,6 +129,7 @@ function drop_table {
 	else
 		echo -e $Red"Table not found"$DefaultColor
 	fi
+	tables_menu
 }
 
 function list_databases { 
@@ -97,20 +143,26 @@ function list_databases {
 }
 
 function connect_database {
-	# Just trial Not the function we want
 	echo "Enter name of the database you want to Use: "
 	read dbName
 	cd ~/Bash_project/$dbName 2>> /dev/null	
 	if [[ $? == 0 ]] #for checking if it's exist or not
 	then 		
-		echo "Connected to $dbName database"
-		clear
+		echo "Connected to $dbName successfully"
+		tables_menu
+	else
+		echo "Database $dbName wasn't found"
+		main_menu
+	fi
+}
+
+function tables_menu {
 		select choice in 'Create Table' 'List Tables' 'Drop Table' 'Insert into Table' 'Select From Table' 'Delete From Table' 'Update Table' 'Back' 'Exit'
 		do
 		case $REPLY in
 			1) create_table
 				;;
-			2) list_tables
+			2) echo "*************" ; ls .; echo "*************"; tables_menu
 				;;
 			3) drop_table
 				;;
@@ -122,16 +174,13 @@ function connect_database {
 				;;
 			7) #update_table
 				;;
-			8) #need to return to main menu
+			8) clear ; main_menu
 				;;
 			9) exit
 				;;
 			*) echo "Invaild choice"
 		esac
-		done	
-	else
-		echo "Database $dbName not exists"
-	fi
+		done
 }
 
 function drop_database { 
@@ -144,21 +193,26 @@ function drop_database {
 	else
 		echo "Database not found"
 	fi
+	main_menu
 }
 
-select choice in 'Create Database' 'List Databases' 'Connect To Database' 'Drop Databse' 'Exit'
-do
-case $REPLY in
-	1) create_database
-		;;
-	2) list_databases
-		;;
-	3) connect_database
-		;;
-	4) drop_database
-		;;
-	5) exit 
-		;;
-	*) echo "Invaild choice"
-esac
-done
+function main_menu {
+	select choice in 'Create Database' 'List Databases' 'Connect To Database' 'Drop Databse' 'Exit'
+	do
+	case $REPLY in
+		1) create_database
+			;;
+		2) list_databases
+			;;
+		3) connect_database
+			;;
+		4) drop_database
+			;;
+		5) exit 
+			;;
+		*) echo "Invaild choice"
+	esac
+	done
+}
+
+main_menu
